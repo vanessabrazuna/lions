e43c5f747e42a1d885b78cb7a3821398c7dc2a2f
vanessabrazuna <brazuna.nessa@gmail.com>
2026-01-21 14:45:22 -0300

    fix: ensure next.config.js is in root

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const { withContentlayer } = require('next-contentlayer');
+import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = withContentlayer(nextConfig);
+export default withContentlayer(nextConfig);
