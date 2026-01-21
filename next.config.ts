68876575a07b3c43dab498178fd2804bfc80391b
vanessabrazuna <brazuna.nessa@gmail.com>
2026-01-21 14:33:01 -0300

    chore: convert next.config.ts to JS for Netlify build

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next';
-import { withContentlayer } from 'next-contentlayer';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
-
-export default withContentlayer(nextConfig);
